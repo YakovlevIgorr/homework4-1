f1db93cf398868f7446fdbfcd948ef7bd0798f36
Igor <jaknikig@mail.ru>
2023-08-03 23:57:17 +0600

    finished

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+class Adress{
+public:
+    std::string city;
+    std::string street;
+    int number_house;
+    int number_flat;
+
+};
+
+class Read{
+
+    int cicle = 0;
+public:
+    void work_to_files(std::string file_in, std::string file_out){
+        std::ifstream in;
+        std::ofstream out;
+        out.open(file_out);
+        in.open(file_in);
+        in >> cicle;
+        Adress adress[cicle];
+        for(int i = 0; i < cicle; i++){
+            in >> adress[i].city;
+            in >> adress[i].street;
+            in >> adress[i].number_house;
+            in >> adress[i].number_flat;
+        }
+        out  << cicle << "\n";
+        for(int i = cicle - 1; i >= 0; i--){
+            out << adress[i].city << ", ";
+            out << adress[i].street << ", ";
+            out << adress[i].number_house << ", ";
+            out << adress[i].number_flat << "\n";
+        }
+    }
+
+};
+int main() {
+
+    Read read;
+    read.work_to_files("in.txt", "out.txt");
+
+    return 0;
+}
